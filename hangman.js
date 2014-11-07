54104b75e687f000bce349106c77cf9ad9edfc46
Will Parker <Will.Parker@widerplan.com>
2014-11-07 16:12:47 +0000

    Added basic function to create a game

--- hangman.js
+++ hangman.js
@@ -1,23 +1,47 @@
+Games = new Meteor.Collection('games');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
+  Template.gamesList.helpers({
+    activeGames: function() {
+      return Games.find({end_time: {$gte: new Date}});
+    },
+  })
+  Template.gamesList.events({
+    'click #newGame': function() {
+      Meteor.call('newGame', function() {
+        alert('new game started');
+      });
     }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
+  })
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  Meteor.methods({
+    newGame: function () {
+      var start = moment();
+      var end = moment().add(7, 'minutes');
+      var phrase = 'This is the phrase';
+      var words = [ {length: 4}, {length: 2}, {length: 3}, {length: 8} ];
+      var hangman_phrase = {
+        phrase: 'This is the sentence',
+        words: words
+      };
+      var game = {
+        host: Meteor.userId,
+        hangman_phrase: hangman_phrase,
+        players: 1,
+        start_time: start.toDate(),
+        end_time: end.toDate(),
+        status: 0
+      };
+      Games.insert(game, function(error) {
+        if (error) {
+          console.log(error);
+          return false;
+        } else {
+          return true;
+        }
+      });
+    }
+  })
 }
