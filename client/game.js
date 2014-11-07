cdda58a0d415803f090e51fd5efe7e32d2d751a8
Will Parker <Will.Parker@widerplan.com>
2014-11-07 17:57:45 +0000

    Added code to show letters

--- client/game.js
+++ client/game.js
@@ -1,5 +1,12 @@
 Template.game.helpers({
   blah: function() {
     console.log(Iron.controller()); // Games.findOne({_id:});
+  },
+  lettersList: function(letters_guessed) {
+  	alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    for(i = 1; i <= alphabet.length; i++) {
+      var letters[] = {letter: alphabet[i], value: (alphabet[i] in letters_guessed ? true : false)};
+    }
+    return letters;
   }
 })
